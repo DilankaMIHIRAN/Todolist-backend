7a38516921ffbe8bd1dc7992233af07687bea512
DilankaMIHIRAN <e1841376@bit.mrt.ac.lk>
2024-03-19 14:30:08 +0530

    auth

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import cors from "cors";
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
 
 //connect to database
 mongoose
@@ -15,6 +18,8 @@ mongoose
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
+
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000 💖");
---
+++ src/routes/MyUserRoute.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+//localhost:8000/api/my/user
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+http: router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentuser
+);
+
+export default router;
